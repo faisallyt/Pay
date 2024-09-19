fd436bb2635d36900c37a280426efd2b510c85a4
faisallyt <fdiledu@gmail.com>
2024-09-20 04:09:40 +0530

    Implement Transaction

--- backend/controllers/AccountControllers/Index.js
+++ backend/controllers/AccountControllers/Index.js
@@ -1,20 +1,61 @@
+const { default: mongoose } = require("mongoose");
 const { Account } = require("../../schema/Account");
 const { User } = require("../../schema/User");
 
 const transaction = async (req, res) => {
-  const { receiver, sender, amount } = req.body;
+  const { receiver, amount } = req.body;
+  const userId = req.userId;
+  console.log(userId);
+  console.log("hello");
+  const session = await mongoose.startSession();
   try {
-    const senderUser = await User.findById(sender);
+    session.startTransaction();
+    const senderUser = await User.findById(userId).session(session);
     if (!senderUser) {
+      await session.abortTransaction();
       return res.status(404).json({ message: "sender not found" });
     }
 
-    const receiverUser = await User.findById(receiver);
+    const receiverUser = await User.findById(receiver).session(session);
     if (!receiverUser) {
+      await session.abortTransaction();
       return res.status(404).json({ message: "receiver not found" });
     }
 
-    const senderAccount = await Account.findOne({ userId: senderUser._id });
+    const senderAccount = await Account.findOne({
+      userId: senderUser._id,
+    }).session(session);
+
+    if (!senderAccount) {
+      await session.abortTransaction();
+      return res.status(404).json({ message: "sender account not found" });
+    }
+
+    if (senderAccount.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Insufficient balance" });
+    }
+
+    const receiverAccount = await Account.findOne({ userId: receiver }).session(
+      session
+    );
+
+    if (!receiverAccount) {
+      await session.abortTransaction();
+      return res.status(404).json({ message: "Receiver account not found" });
+    }
+
+    await Account.updateOne(
+      { userId: userId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+    await Account.updateOne(
+      { userId: receiver },
+      { $inc: { balance: amount } }
+    ).session(session);
+
+    await session.commitTransaction();
+    return res.status(200).json({ message: "Transaction Successfully" });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "server error", error });
--- backend/routes/Account.js
+++ backend/routes/Account.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { getBalance } = require("../controllers/AccountControllers/Index");
+const {
+  getBalance,
+  transaction,
+} = require("../controllers/AccountControllers/Index");
 
 const router = express.Router();
 
 router.get("/balance", authMiddleware, getBalance);
+router.put("/transaction", authMiddleware, transaction);
 
 module.exports = router;
